a9806d8979ae24e3535fd60237bec1050319cf13
Haneefah Witherspoon <haneefah207@gmail.com>
2025-01-02 02:12:47 -0500

    initial todo styling, started add item feature, work on delete item next and content box

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const [searchInput, setSearchInput] = useState('');
+
+  const addItem = (e) => {
+    setSearchInput(e.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setTodos([...todos, searchInput])
+    setSearchInput('')
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="w-screen h-screen bg-slate-900 flex justify-center items-center">
+      <div className="todoContainer px-12 py-10 h-3/5 max-h-full bg-gradient-to-br from-gray-800 to-gray-900 rounded-lg shadow-md shadow-slate-950">
+        <form onSubmit={handleSubmit}>
+          <input 
+          type="text" 
+          className="py-2 px-2 bg-transparent border  text-gray-100 transition-all"
+          placeHolder="Item here..."
+          value={searchInput}
+          onChange={addItem}/>
+          <button className="text-white uppercase text-sm px-5 py-2 ms-10 border hover:shadow-md hover:shadow-green-700 transition-all tracking-widest">Add Todo</button>
+        </form>
+
+        <ul className="mt-10 flex flex-col flex-wrap">
+          {todos.map((todo) => (
+            <li 
+            key={todo}
+            className="text-slate-50 px-2 border-b border-slate-500 pb-3 pt-3">{todo}
+            <button 
+            className="uppercase tracking-widest text-sm ms-5">X</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
   );
+
+
 }
 
 export default App;
---
+++ src/components/TodoContainer.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+
+const TodoContainer = () => {
+    return (
+        <>
+        
+        </>
+    )
+
+}
+
+export default TodoContainer;
