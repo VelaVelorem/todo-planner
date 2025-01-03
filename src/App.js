91cd3d5d8f1507578e10548102548b2b30c603f3
Haneefah Witherspoon <haneefah207@gmail.com>
2025-01-03 02:28:04 -0500

    added second section, finished styling for todo list, added delete task feature, work on second section next (make separate file for it)

--- src/App.js
+++ src/App.js
@@ -11,36 +11,60 @@ function App() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    
+    if(todos.length >= 10) {
+      alert('Woops! You can only add a maximum of 10 items to the list. Please delete a task before adding a new one.')
+      return;
+    }
+
+    if(searchInput.trim() === '') {
+      alert("Oops, you need to add a valid task!")
+      return;
+    }
+      
     setTodos([...todos, searchInput])
     setSearchInput('')
   }
 
-
+  const handleDelete = (index) => {
+    const todoCopy = [...todos];
+    todoCopy.splice(index, 1);
+    setTodos(todoCopy)
+  }
 
   return (
-    <section className="w-screen h-screen bg-slate-900 flex justify-center items-center">
-      <div className="todoContainer px-12 py-10 h-3/5 max-h-full bg-gradient-to-br from-gray-800 to-gray-900 rounded-lg shadow-md shadow-slate-950">
-        <form onSubmit={handleSubmit}>
-          <input 
-          type="text" 
-          className="py-2 px-2 bg-transparent border  text-gray-100 transition-all"
-          placeHolder="Item here..."
-          value={searchInput}
-          onChange={addItem}/>
-          <button className="text-white uppercase text-sm px-5 py-2 ms-10 border hover:shadow-md hover:shadow-green-700 transition-all tracking-widest">Add Todo</button>
+
+    <section className="w-screen h-screen bg-slate-900 flex justify-evenly flex-wrap items-center border-b border-slate-400">
+      <div className="todoContainer px-12 py-10 h-3/5 max-h-full  bg-gradient-to-br from-gray-800 to-gray-900 rounded-lg shadow-lg shadow-slate-950">
+        <form 
+        onSubmit={handleSubmit}
+        className="flex gap-5">
+          <input
+            type="text"
+            className="py-2 px-2 bg-transparent border  text-gray-100 transition-all"
+            placeHolder="Item here..."
+            value={searchInput}
+            onChange={addItem} />
+          <button className="text-white uppercase text-sm px-5 py-2 border hover:shadow-md hover:text-green-300 hover:border-green-300 transition-all tracking-widest">Add Todo</button>
         </form>
 
         <ul className="mt-10 flex flex-col flex-wrap">
           {todos.map((todo) => (
-            <li 
-            key={todo}
-            className="text-slate-50 px-2 border-b border-slate-500 pb-3 pt-3">{todo}
-            <button 
-            className="uppercase tracking-widest text-sm ms-5">X</button>
+            <li
+              key={todo}
+              className="text-slate-50 px-2 border-b border-slate-500 pb-3 pt-3  flex items-center justify-between text-wrap hover:bg-slate-500 hover:bg-opacity-40">{todo}
+              <button
+                className="uppercase tracking-widest text-sm ms-5"
+                onClick={handleDelete}>X</button>
             </li>
           ))}
         </ul>
       </div>
+
+      <aside className="border p-10 order-first">
+        <h1
+        className="text-3xl text-slate-100">To Do List</h1>
+      </aside>
     </section>
   );
 
